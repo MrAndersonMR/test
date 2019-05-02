28a62acf2b3f0f591ab44c5e2aa9698e5cac3dfa
Anderson.Bession <amendes@beesion.com>
2019-05-02 15:05:01 -0300

    new function

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,5 +1,15 @@
 package test;
 
-public class Test {
-	Anderson
+class test {
+	public static void main(String args[]) {
+		int array[][] = new int[3][3];
+		
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				array[i][j]=i+1;
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
